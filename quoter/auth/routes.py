c633d1c976c976daba25daadb87f463910cebe20
Alex Ho <ho.alex100@gmail.com>
2023-03-25 10:25:34 -0400

    add navbar

--- quoter/auth/routes.py
+++ quoter/auth/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, flash
 from flask_login import current_user, login_user, login_required, logout_user
 
 from quoter import login_manager, bcrypt, db
@@ -15,36 +15,42 @@ def load_user(user_id):
 
 @auth.route("/register", methods=["GET", "POST"])
 def register():
+    # TODO: Make decorator for this. Users can only access register and login page if logged out
+    if current_user.is_authenticated:
+        return redirect(url_for("root.index"))
+
     form = RegisterForm()
-    if not current_user.is_authenticated:
-        if form.validate_on_submit():
-            email = form.email.data
-            username = form.username.data
-            first_name = form.first_name.data
-            last_name = form.last_name.data
-            password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-            user = User(email=email, username=username, first_name=first_name, last_name=last_name, password=password)
-            db.session.add(user.id)
-            db.session.commit()
-            return redirect(url_for("root.index"))
+    if form.validate_on_submit():
+        email = form.email.data
+        username = form.username.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(email=email, username=username, first_name=first_name, last_name=last_name, password=password)
+        db.session.add(user.id)
+        db.session.commit()
+        return redirect(url_for("root.index"))
     return render_template("auth/register.html", form=form)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    # TODO: Replace with a custom decorator.
+    if current_user.is_authenticated:
+        return redirect(url_for("root.index"))
+
     form = LoginForm()
-    if not current_user.is_authenticated:
-        if form.validate_on_submit():
-            user = User.query.filter_by(email=form.email.data).first()
-            if user is not None:
-                if bcrypt.check_password_hash(user.password, form.password.data):
-                    login_user(user)
-                else:
-                    # TODO: Handle incorrect password
-                    pass
-            else:
-                # TODO: Handle incorrect username
-                pass
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
                 return redirect(url_for("root.index"))
+            else:
+                # TODO: Handle incorrect password
+                pass
+        else:
+            # TODO: Handle incorrect username
+            pass
     return render_template("auth/login.html", form=form)
 
 @auth.route("/logout", methods=["GET", "POST"])
